7b156d7421912fd308287dac6a03812a6b0d1d61
Veronika Novakova <veronika3novak@gmail.com>
2021-04-13 18:50:52 +0200

    [FEATURE] main function of sportka

--- sportka.js
+++ sportka.js
@@ -6,3 +6,26 @@
 // Z každého vylosovaného čísla vygeneruj následující HTML kód:
 // <span class="cislo">8</span>
 // ... který pak přidej dovnitř prvku <div id="vyherni-cisla">:
+
+let osudi = [];
+
+for (let i=1; i<=48; i=i+1) {
+    osudi.push(i);
+}
+
+console.log(osudi);
+
+//z osudí vyberu 7 náhodných čísel 
+
+let tazenaCisla = [];
+
+for (let i = 0; i < 7; i++) {
+    let vyherniIndex = Math.floor(Math.random() * osudi.length);
+    let VyherniCislo = osudi[vyherniIndex];
+
+    tazenaCisla.push(VyherniCislo);
+    osudi.splice(vyherniIndex, 1);
+
+}
+
+//a vypíšu je do html
